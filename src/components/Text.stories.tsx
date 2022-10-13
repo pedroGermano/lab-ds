1c20a657bda8fbb2a6f73d9e682d660434596568
Pedro Germano <pedrogermano232@gmail.com>
2022-10-13 10:14:50 -0300

    Arg Types

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text';
+
+export default {
+  title: 'Component/Text',
+  component: Text,
+  size: 'md',
+  args: {
+    children: 'Lorem ipsum.'
+  },
+  argTypes:{
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testando</p>
+    )
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      }
+    }
+  }
+}
