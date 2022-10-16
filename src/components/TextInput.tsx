1eb57301cef41bedabab0adb2a66487c9db17c00
Pedro Germano <pedrogermano232@gmail.com>
2022-10-16 18:52:30 -0300

    test ok

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,6 +7,7 @@ export function TextInput(props: TextInputProps) {
   return (
     <div className="py-4 px-3 rounded bg-gray-800 w-full text-gray-100 outline-none text-xs placeholder:text-gray-400 focus:ring-2 ring-cyan-300">
       <input className={clsx("")} {...props} />
+      <input className={clsx("")} {...props} />
     </div>
   );
 }
