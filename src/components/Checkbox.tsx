28935bb650864b0b60f994ce232a2a71fb5939bf
Pedro Germano <pedrogermano232@gmail.com>
2022-10-18 07:54:10 -0300

    CheckBox start

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,13 @@
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+export interface CheckboxProps{
+}
+
+export function Checkbox({   }: CheckboxProps){
+  return (
+    <div className=""
+
+    />
+  )
+}
